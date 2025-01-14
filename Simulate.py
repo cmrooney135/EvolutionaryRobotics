9086c8a13f2abefa90beb51ef5ab4cf6623eee11
cmrooney135 <cmrooney@uvm.edu>
2025-01-14 12:15:06 -0500

    complete Simulate.py setup

--- Simulate.py
+++ Simulate.py
@@ -1,3 +1,11 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range (1000):
+    p.stepSimulation()
+    time.sleep(0.05)
+    print(i)
+
 p.disconnect()
