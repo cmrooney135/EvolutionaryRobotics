a5ead4f55fb690135d0745e0a6453988c78aa3ea
cmrooney135 <cmrooney@uvm.edu>
2025-01-21 12:38:47 -0500

    commit sim and generate

--- Simulate.py
+++ Simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-
+p.loadSDF("box.sdf")
 for i in range (1000):
     p.stepSimulation()
     time.sleep(0.05)
