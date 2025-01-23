6d8373e510cd21aed90818fea29338799d09e11f
cmrooney135 <cmrooney@uvm.edu>
2025-01-23 10:24:28 -0500

    update generate and simulate for multiple links

--- Simulate.py
+++ Simulate.py
@@ -1,11 +1,18 @@
 import pybullet as p
 import time
-physicsClient = p.connect(p.GUI)
-#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+import pybullet_data
 
-for i in range (1000):
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-10, physicsClient)
+
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
+
+for i in range (10000):
     p.stepSimulation()
-    time.sleep(0.05)
+    time.sleep(0.004)
     print(i)
 
 p.disconnect()
---
+++ generate.py
@@ -0,0 +1,9 @@
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("boxes.sdf")
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1, 1, 1])
+pyrosim.Send_Cube(name="Box2", pos=[0,0,0.5] , size=[1, 1, 1])
+
+
+pyrosim.End()
