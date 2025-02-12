89635bbf856ba4bf9de97a632998574ab8be98d2
cmrooney135 <cmrooney@uvm.edu>
2025-02-12 11:10:18 -0500

    first half of learning objective motors

--- Simulate.py
+++ Simulate.py
@@ -2,7 +2,9 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
-import numpy
+import numpy as np
+import random as rand
+
 
 
 physicsClient = p.connect(p.GUI)
@@ -16,15 +18,38 @@ robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotID)
-backLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = np.zeros(100)
+frontLegSensorValues = np.zeros(100)
+
+targetAngles = np.linspace(0,2*np.pi, 100)
+np.save("data/targetAngles.npy", targetAngles)
+exit()
 
 
-for i in range (1000):
+for i in range (100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backLeg")
-    time.sleep(0.004)
     print(i)
+    print("backleg sensor val: ")
+    print(backLegSensorValues[i])
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontLeg")
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotID,
+        jointName=b"torso_backLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=(rand.random() * np.pi) - (np.pi / 2),
+        maxForce=50)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotID,
+        jointName=b"torso_frontLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=(rand.random() * np.pi) - (np.pi / 2),
+        maxForce=50)
+
+    time.sleep(0.004)
+
 
 p.disconnect()
-print("Back leg sensor values: ")
-print(backLegSensorValues)
+np.save("data/back_leg_sensor_values.npy", backLegSensorValues)
+np.save("data/front_leg_sensor_values.npy", frontLegSensorValues)
--- analyze.py
+++ analyze.py
@@ -1,8 +1,20 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-from Simulate import backLegSensorValues
 
-backLegSensorValues = numpy.load(backLegSensorValues.npy)
+#backLegSensorValues = np.load("data/back_leg_sensor_values.npy")
+#frontLegSensorValues = np.load("data/front_leg_sensor_values.npy")
+targetAngles = np.load("data/targetAngles.npy")
 
-plt.plot(backLegSensorValues)
+
+
+#plt.plot(backLegSensorValues, label="back Leg sensor values", linewidth=3)
+#plt.plot(frontLegSensorValues, label="front Leg sensor values")
+#plt.title("Back leg and front leg sensor values")
+#plt.legend()
+#plt.savefig("plot.png", dpi=300, bbox_inches='tight')
+#plt.show()
+
+plt.plot(targetAngles, np.sin(targetAngles))
+plt.title("sin Values")
+plt.savefig("sinValues.png", dpi=300, bbox_inches='tight')
 plt.show()
