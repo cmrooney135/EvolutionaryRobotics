524df461afbd3952fcb53f2863f2cfb29d8e0c1d
cmrooney135 <cmrooney@uvm.edu>
2025-03-02 18:35:38 -0500

    steps 1-67

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.End()
 
-def Genetate_Body():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="torso_frontLeg", parent="torso", child="frontLeg",
@@ -16,11 +16,23 @@ def Genetate_Body():
 
     pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name=0, linkName="torso")
+    pyrosim.Send_Sensor_Neuron(name=1, linkName="backLeg")
+    pyrosim.Send_Sensor_Neuron(name=2, linkName="frontLeg")
+    pyrosim.Send_Motor_Neuron(name=3, jointName="torso_backLeg")
+    pyrosim.Send_Motor_Neuron(name=4, jointName="torso_frontLeg")
+
+
+    pyrosim.End()
+
 def Create_Robot():
    pass
 def main():
     Create_World()
-    Genetate_Body()
+    Generate_Body()
+    Generate_Brain()
 
 
 if __name__ == "__main__":
